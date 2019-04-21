16f647955759d9518665d3ab92a36fb4eb9c102e
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-21 17:31:20 -0400

    Adding the cors module

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var events = require('events');
 
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var app = express();
+
+app.use(cors());
 
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {
