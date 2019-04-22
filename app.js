8bb104e305e1b43bb02f8ae035a76e3c0d30a339
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-22 00:59:52 -0400

    Slowing this calculation down

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ function CalculationPerformer(velocity) {
 function performCalculation(velocity) {
   calculation = new CalculationPerformer(velocity);
   calculation = null;
-  setTimeout(performCalculation, 150, velocity);
+  setTimeout(performCalculation, 350, velocity);
 }
 
 velocities = [1e-2, 1e-1, 1, 1e1, 1e2];
